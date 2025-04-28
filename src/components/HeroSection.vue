8baac1f00b9d3bcf64e37e2eee63d81477444d8e
renggamaulana <renggaa21@gmail.com>
2025-04-28 11:58:18 +0700

    update hero section

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="p-10 flex justify-between gap-20">
+        <img src="../assets/hello.jpg" class="w-1/3 rounded-4xl shadow-lg" alt="">
+        <div class="w-2/3 flex flex-col">
+            <h1 class="text-6xl font-extrabold tracking-tight leading-17 text-transparent bg-clip-text bg-gradient-to-r from-teal-400 via-blue-500 to-indigo-600 mb-6">
+                Catalyzing Your Digital Transformation
+                <span class="text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-teal-500 to-blue-600 shadow-lg">
+                with Katalis Dev
+                </span>
+            </h1>
+            <button class="px-8 mt-10 flex items-center gap-2 self-baseline py-3 rounded-full bg-gradient-to-r from-teal-500 via-sky-400 to-indigo-600 text-white font-semibold text-lg shadow-lg hover:opacity-80 transition-opacity duration-300 transform hover:scale-105 cursor-pointer">
+                <span>Get Started</span>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                    <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm4.28 10.28a.75.75 0 0 0 0-1.06l-3-3a.75.75 0 1 0-1.06 1.06l1.72 1.72H8.25a.75.75 0 0 0 0 1.5h5.69l-1.72 1.72a.75.75 0 1 0 1.06 1.06l3-3Z" clip-rule="evenodd" />
+                </svg>
+            </button>
+
+        </div>
+    </div>
+</template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+    <nav class="flex justify-between p-4 shadow items-center">
+        <h1 class="text-2xl">
+            <a href="">Katalis Dev</a>
+        </h1>
+        <ul class="flex gap-4">
+            <li><a href="">Home</a></li>
+            <li><a href="">Services</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+    </nav>
+</template>
