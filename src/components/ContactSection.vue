28f114b256a6cb83e2b2d2d05da6dfaab48ce4ad
Rengga <renggaa21@gmail.com>
2025-05-11 07:24:44 +0700

    update

--- src/components/ContactSection.vue
+++ src/components/ContactSection.vue
@@ -1,7 +1,7 @@
 <template>
   <section id="contact" class="min-h-screen flex items-center justify-center px-6 py-20" style="background: radial-gradient(circle at top left, #0f172a, #000000);">
     <div class="max-w-4xl w-full text-center">
-      <h2 class="text-4xl md:text-5xl font-extrabold mb-6 py-2 bg-clip-text text-transparent bg-gradient-to-r from-teal-400 via-blue-500 to-indigo-600"  data-aos="fade-up" 
+      <h2 class="text-4xl md:text-5xl font-extrabold mb-6 py-2 bg-clip-text text-transparent bg-gradient-to-r from-teal-400 via-blue-500 to-indigo-600" data-aos="fade-up" 
       data-aos-duration="1000">
         {{ $t("contact.title") }}
       </h2>
@@ -10,7 +10,7 @@
       </p>
 
       <!-- Form -->
-      <form v-if="showForm" @submit="sendEmail" class="grid grid-cols-1 md:grid-cols-2 gap-6 bg-white/10 backdrop-blur-md p-8 rounded-2xl shadow-lg"  data-aos="fade-up" data-aos-duration="1400">
+      <form v-if="showForm" @submit="sendEmail" class="grid grid-cols-1 md:grid-cols-2 gap-6 bg-white/10 backdrop-blur-md p-8 rounded-2xl shadow-lg" data-aos="fade-up" data-aos-duration="1400">
         <div class="flex flex-col gap-2">
           <label for="name" class="text-left text-gray-400 text-sm">{{ $t("contact.name") }}</label>
           <input
